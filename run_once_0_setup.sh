c9b5b7a7684f3d86f45dc5becde3d6922eb0db85
antonin bruneau <antonin.bruneau@gmail.com>
2020-05-15 16:17:05 +0200

    isolate osx config for future needs

--- run_once_0_setup.sh
+++ run_once_0_setup.sh
@@ -1,47 +1,50 @@
 #!/bin/sh
 
-echo "Setting up your Mac..."
+setutMac() {
+    echo "Setting up your Mac..."
+    if type xcode-select >&- && xpath=$(xcode-select --print-path) &&
+        test -d "${xpath}" && test -x "${xpath}"; then
+        echo "Command Line Tool already installed"
+    else
+        xcode-select --install
+    fi
 
-if type xcode-select >&- && xpath=$(xcode-select --print-path) &&
-    test -d "${xpath}" && test -x "${xpath}"; then
-    echo "Command Line Tool already installed"
-else
-    xcode-select --install
+    # Check for Homebrew and install if we don't have it
+    if test ! $(which brew); then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
+    # Update Homebrew recipes
+    brew update
+
+    # Install all our dependencies with bundle (See Brewfile)
+    brew tap homebrew/bundle
+    brew bundle --no-lock --no-upgrade
+
+    # Setup Iterm
+    wget http://ethanschoonover.com/solarized/files/solarized.zip && unzip solarized.zip
+    open solarized/iterm2-colors-solarized/Solarized\ Dark.itermcolors
+    rm -rf solarized && rm solarized.zip
+
+    # Font
+    wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P /Library/Fonts
+    wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -P /Library/Fonts
+    wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -P /Library/Fonts
+    wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -P /Library/Fonts
+
+    # Setup VSCode
+    source $(chezmoi source-path)/.vscode
+
+    # Store MD5s
+    echo "BREWFILE=$(md5 -q $(chezmoi source-path)/Brewfile)" >>~/.dotfile
+    echo "VSCODE=$(md5 -q $(chezmoi source-path)/.vscode)" >>~/.dotfile
+    echo "MACOS=$(md5 -q $(chezmoi source-path)/.macos)" >>~/.dotfile
+
+    # Set macOS preferences
+    # We will run this last because this will reload the shell
+    source $(chezmoi source-path)/.macos
+}
+
+if [ "$(uname)" == "Darwin" ]; then
+    setutMac
 fi
-
-# Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-# Update Homebrew recipes
-brew update
-
-HOMEBREW_NO_AUTO_UPDATE=1
-
-brew tap homebrew/cask
-brew tap homebrew/cask-eid
-brew tap homebrew/cask-fonts
-brew tap homebrew/cask-versions
-brew tap homebrew/bundle
-
-brew install tree || true
-brew install go || true
-brew install k9s || true
-brew install kubectx || true
-brew install wget || true
-brew install zsh-autosuggestions || true
-brew install zsh-syntax-highlighting || true
-brew install telnet || true
-brew install romkatv/powerlevel10k/powerlevel10k || true
-brew install terminal-notifier || true
-
-brew cask install iterm2 || true
-brew cask install docker || true
-brew cask install font-fira-code || true
-brew cask install macdown || true
-brew cask install visual-studio-code || true
-brew cask install firefox || true
-brew cask install karabiner-elements || true
-brew cask install rectangle || true
-brew cask install keepingyouawak || true
---
+++ run_update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+updateMac() {
+    echo "Updating your Mac..."
+
+    if cat ~/.dotfile | grep -q $(md5 -q $(chezmoi source-path)/Brewfile); then
+        echo "Brew up to date"
+    else
+        echo "Updating Brewfile..."
+        brew bundle --no-lock --no-upgrade
+        sed -i.back "s/^BREWFILE=.*/BREWFILE=$(md5 -q $(chezmoi source-path)\/Brewfile)/" ~/.dotfile
+    fi
+
+    if cat ~/.dotfile | grep -q $(md5 -q $(chezmoi source-path)/.vscode); then
+        echo "VSCode up to date"
+    else
+        echo "Updating VSCode pluggins ..."
+        source .vscode
+        sed -i.back "s/^VSCODE=.*/VSCODE=$(md5 -q $(chezmoi source-path)\/.vscode)/" ~/.dotfile
+    fi
+
+    if cat ~/.dotfile | grep -q $(md5 -q $(chezmoi source-path)\/.macos); then
+        echo "Mac OS config up to date"
+    else
+        echo "Updating Mac OS config"
+        source .macos
+        sed -i.back "s/^MACOS=.*/MACOS=$(md5 -q $(chezmoi source-path)\/.macos)/" ~/.dotfile
+    fi
+}
+
+if [ "$(uname)" == "Darwin" ]; then
+    updateMac
+fi
